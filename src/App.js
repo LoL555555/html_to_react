0ae58fc9ce0fb426c028de2e4257dcb694ead9e4
manaschubby <37976250+manaschubby@users.noreply.github.com>
2023-03-29 15:43:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
 						BITS Pilani, Hyderabad Campus
 					</p>
 			    </a>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-									controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"									controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 			  	</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
